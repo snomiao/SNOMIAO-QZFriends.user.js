3d500c52c6a6fca33b3e4b42f13b33cc12a6fcc7
snomiao <snomiao@gmail.com>
2020-07-13 20:55:16 +0800

    修剪

--- index.js
+++
@@ -1,7 +0,0 @@
-var _callback = (e) => {
-    var 元 = e.data;
-    var 群名 = Object.entires(元.gpnames.map(({ gpid, gpname }) => [gpid, gpname]))
-    var 输出 = 元.items.map(
-        ({ name, remark, uin, groupid }) => [uin, 群名[groupid], name, remark].replace('\t').join('\t'))
-    console.log(输出)
-}
